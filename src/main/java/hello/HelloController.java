06669048e8c36857752407d7d34fa995feb435bb
umaprasath udaiyar <umaprasath.lru@gmail.com>
2015-12-05 23:17:03 +1100

    second commit

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
 
-        return "Greetings !! spring boot is up and running";
+        return "Greetings spring boot is up and running";
     }
     
 }
